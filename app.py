d268e9e4997b0cae272766647ec1fdcba59bf2ea
Kulbanu <r.r.r.r.4.g.a@gmail.com>
2024-06-06 17:46:15 +0500

    fix cors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 cors = CORS(app)
-CORS(app, resources={r"/*": {"origins": ["https://anamenbala.netlify.app/"]}})
+CORS(app, resources={r"/*": {"origins": ["https://anamenbala.netlify.app"]}})
 model = genai.GenerativeModel('gemini-1.5-flash')
 genai.configure(api_key='AIzaSyC4IfeZfDy-ZgsgGp9od8Xf3jBKmLjtut8')
 @app.route('/api/answer', methods=['POST'])
@@ -15,7 +15,7 @@ def answer():
     prompt = data['prompt']
     response = model.generate_content(prompt)
     result = jsonify({'response': response.text})
-    result.headers.add('Access-Control-Allow-Origin', 'https://anamenbala.netlify.app/')
+    result.headers.add('Access-Control-Allow-Origin', 'https://anamenbala.netlify.app')
     result.headers.add('Access-Control-Allow-Credentials', 'true')
     return result
 
