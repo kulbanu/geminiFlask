cd5985a5951877bc3b93f0130122eb410ff53c8d
Kulbanu <r.r.r.r.4.g.a@gmail.com>
2024-06-06 17:17:45 +0500

    fix cors

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import google.generativeai as genai
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/answer*": {"origins": "https://anamenbala.netlify.app/"}})
+cors = CORS(app)
+CORS(app, resources={r"/api/answer*": {"origins": ["https://anamenbala.netlify.app/"]}})
 model = genai.GenerativeModel('gemini-1.5-flash')
 genai.configure(api_key='AIzaSyC4IfeZfDy-ZgsgGp9od8Xf3jBKmLjtut8')
 @app.route('/api/answer', methods=['POST'])
